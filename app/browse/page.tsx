c4d32c5caf14d7d7246da84a3be04a276c3b8430
Yatharth <yatharth0903@gmail.com>
2024-05-07 15:46:06 +0530

    update

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -2,13 +2,15 @@ import RoomCard from "@/components/RoomCard";
 import { SearchBar } from "@/components/search-bar";
 import { Button } from "@/components/ui/button";
 import { getRooms } from "@/data-access/rooms";
+import Image from "next/image";
 import Link from "next/link";
 
 
-export default async function Home({searchParams}:{searchParams:{
-  search:string
-}}) 
-{
+export default async function Home({ searchParams }: {
+  searchParams: {
+    search: string
+  }
+}) {
   const rooms = await getRooms(searchParams.search);
   return (
     <main className=" min-h-screen  p-16">
@@ -19,17 +21,31 @@ export default async function Home({searchParams}:{searchParams:{
         </Button>
       </div>
       <div className="mb-12">
-        <SearchBar/>
+        <SearchBar />
       </div>
-      
+
 
       <div className="grid md:grid-cols-3 gap-4 grid-cols-1">
         {rooms.map((room) => {
           return (
-             <RoomCard key={room.id} room={room} />
+            <RoomCard key={room.id} room={room} />
           )
         })}
       </div>
+
+      {rooms.length === 0 && (
+        <div className="flex flex-col gap-4 justify-center items-center mt-24">
+          <Image
+            className="rounded-[20px]"
+            src="/notfound.png"
+            width="200"
+            height="200"
+            alt="no data image"
+          />
+
+          <h2 className="text-2xl">No rooms availabel now</h2>
+        </div>
+      )}
 
     </main>
   );
--- app/your-rooms/page.tsx
+++ app/your-rooms/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { getUserRooms } from "@/data-access/rooms";
 import Link from "next/link";
 import RoomCard from "./userroomcard";
+import Image from "next/image";
 
 
 export default async function YourRoomPage() {
@@ -24,6 +25,24 @@ export default async function YourRoomPage() {
           )
         })}
       </div>
+
+      {rooms.length === 0 && (
+        <div className="flex flex-col gap-4 justify-center items-center mt-24">
+          <Image
+          className="rounded-[20px]"
+            src="/notfound.png"
+            width="200"
+            height="200"
+            alt="no data image"
+          />
+
+          <h2 className="text-2xl">You have no rooms</h2>
+
+          <Button asChild>
+            <Link href="/create-room">Create Room</Link>
+          </Button>
+        </div>
+      )}
 
     </main>
   );
